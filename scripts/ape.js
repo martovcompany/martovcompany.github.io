6ed6a06db4c55b1d704ca013fe234e53ee3bf41c
dai-martov <75813091+dai-martov@users.noreply.github.com>
2021-07-15 16:39:35 -0400

    Update ape.js

--- scripts/ape.js
+++ scripts/ape.js
@@ -1,6 +1,8 @@
 import { ethers } from "https://dai-martov.github.io/scripts/ethers-5.2.esm.min.js";
+// let ethers = require("https://dai-martov.github.io/scripts/ethers-5.2.esm.min.js")
 
 const apeAddress = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
+let realURI = ""
 
 async function getBalance(ape) {
     if (typeof window.ethereum !== 'undefined') {
@@ -14,7 +16,7 @@ async function getBalance(ape) {
       } catch (err) {
         console.log("Error: ", err)
       }
-    }    
+    }
 }
 
 async function getNFTs(ape, db) {
@@ -29,9 +31,7 @@ async function getNFTs(ape, db) {
         if (data != '') {
           let imageURI = db[data]
           let id = imageURI.split("//")[1]
-          let realURI = "ipfs.io/ipfs/" + id
-          emitUIInteraction(realURI)
-          console.log(realURI, "emitted URI")
+          realURI = "ipfs.io/ipfs/" + id
         }
       } catch (err) {
         console.log("Error: ", err)
@@ -47,17 +47,15 @@ async function getRes() {
       ]);
     let ape = await apeRes.json()
     let db = await dbRes.json()
-    
-    getBalance(ape)
-    getNFTs(ape, db)
-    
+
+    await getBalance(ape)
+    await getNFTs(ape, db)
+
     //return [ape, db]
 }
 
-// window.onload = function(){ 
-//     setTimeout( function() { 
-//         getRes() 
-//     }
-//    , 5000); 
-// };
+getRes()
 
+setTimeout(function(){
+    emitUIInteraction(realURI)
+}, 4000);
