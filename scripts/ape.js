9b72549f74e5679ae8a241ae506b62cee3089b4a
dai-martov <75813091+dai-martov@users.noreply.github.com>
2021-07-22 11:47:16 -0400

    Update ape.js

--- scripts/ape.js
+++ scripts/ape.js
@@ -2,7 +2,7 @@ import { ethers } from "https://dai-martov.github.io/scripts/ethers-5.2.esm.min.
 // let ethers = require("https://dai-martov.github.io/scripts/ethers-5.2.esm.min.js")
 
 const apeAddress = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
-let realURI = "ipfs.io/ipfs/QmYsWYyQL2rTykTb8a9erJ6cSRRLqpC1sk3NE7n6SbgAaJ"
+let realURI = ""
 
 async function getBalance(ape) {
     if (typeof window.ethereum !== 'undefined') {
