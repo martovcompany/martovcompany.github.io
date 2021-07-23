a97caca3bf5d21e479bbd81401c60c82c66bef36
dai-martov <75813091+dai-martov@users.noreply.github.com>
2021-07-23 15:15:57 +0000

    Update ape.js

--- scripts/ape.js
+++ scripts/ape.js
@@ -2,7 +2,7 @@ import { ethers } from "https://dai-martov.github.io/scripts/ethers-5.2.esm.min.
 // let ethers = require("https://dai-martov.github.io/scripts/ethers-5.2.esm.min.js")
 
 const apeAddress = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
-let realURI = {"ipfs": "No ape"}
+let realURI = {"ipfs": "No ape", "attrs"}
 
 async function getBalance(ape) {
     if (typeof window.ethereum !== 'undefined') {
@@ -26,9 +26,10 @@ async function getNFTs(ape, db) {
         const data = await contract.tokenOfOwnerByIndex(account, 0)
         console.log('NFT: ', data.toString())
         if (data != '') {
-          let imageURI = db[data]
+          let imageURI = db[data]['ipfs']
           let id = imageURI.split("//")[1]
           realURI.ipfs = "ipfs.io/ipfs/" + id
+          realURI.attrs = db[data]["attributes"]
         }
       } catch (err) {
         console.log("Error: ", err)
@@ -40,7 +41,7 @@ async function getRes() {
     let json_data = await fetch('https://dai-martov.github.io/public/Ape.json')
     const [apeRes, dbRes] = await Promise.all([
         fetch('https://dai-martov.github.io/public/Ape.json'),
-        fetch('https://dai-martov.github.io/public/db2.json')
+        fetch('https://dai-martov.github.io/public/db3.json')
       ]);
     let ape = await apeRes.json()
     let db = await dbRes.json()
