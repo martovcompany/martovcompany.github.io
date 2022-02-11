eeef0ba323486df0b397f00b9f22760ecefe7a1f
Kate <61888654+EkaterinaGorbunova@users.noreply.github.com>
2022-02-11 13:54:42 -0500

    Update ape.js

--- scripts/ape.js
+++ scripts/ape.js
@@ -207,8 +207,10 @@ function apeFileRun() {
 }
 apeFileRun()
 
-window.localStorage.getItem('name')
-console.log('name from martoverse localStorage', window.localStorage.getItem('name'))
+console.log('cookies:', document.cookie)
+
+// window.localStorage.getItem('name')
+// console.log('name from martoverse localStorage', window.localStorage.getItem('name'))
 
    // Retrieve all eth user data (as a string) from localStorage
   //  const currentEthData = window.localStorage.getItem('ethData')   
